5985357d1475066f5cc337086d4cc686e3f5570e
bigbetroyal-lab <bigbetroyal@gmail.com>
2025-12-31 20:23:34 +0000

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,12 @@
+const buttons = document.querySelectorAll("aside button");
+const telas = document.querySelectorAll(".tela");
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const target = btn.dataset.tela;
+        telas.forEach(tela => {
+            tela.style.display = tela.id === target ? "block" : "none";
+        });
+    });
+});
 
