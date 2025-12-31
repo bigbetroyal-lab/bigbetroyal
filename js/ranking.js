346840f713018ec75149563b245951627d3b94b8
bigbetroyal-lab <bigbetroyal@gmail.com>
2025-12-31 20:44:32 +0000

    Create ranking.js

---
+++ js/ranking.js
@@ -0,0 +1,18 @@
+import { db } from "./firebase.js";
+import { collection, getDocs, query, orderBy, limit } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+const tabelaRanking = document.querySelector("#ranking-table tbody");
+
+export async function carregarRanking() {
+    tabelaRanking.innerHTML = "";
+    const q = query(collection(db, "users"), orderBy("saldo", "desc"), limit(10));
+    const querySnapshot = await getDocs(q);
+    let i = 1;
+    querySnapshot.forEach(doc => {
+        const data = doc.data();
+        const tr = document.createElement("tr");
+        tr.innerHTML = `<td>${i}</td><td>${doc.id}</td><td>${data.saldo}</td>`;
+        tabelaRanking.appendChild(tr);
+        i++;
+    });
+}
