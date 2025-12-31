fa20ae66c4bd3d6db978f32ca82776e485c9bebc
bigbetroyal-lab <bigbetroyal@gmail.com>
2025-12-31 20:25:36 +0000

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,28 @@
 
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { criarDocumentoUsuario, carregarSaldo, carregarHistorico } from "./saldo.js";
+
+export function setupAuth() {
+    const btnLogout = document.getElementById("btn-logout");
+
+    onAuthStateChanged(auth, async (user) => {
+        const telas = document.querySelectorAll(".tela");
+        if (user) {
+            document.querySelector("aside").style.display = "block";
+            document.getElementById("top-saldo").style.display = "inline";
+            await carregarSaldo(user.uid);
+            await carregarHistorico(user.uid);
+        } else {
+            document.querySelector("aside").style.display = "none";
+            document.getElementById("top-saldo").style.display = "none";
+            telas.forEach(tela => tela.style.display = tela.id === "historico" ? "block" : "none");
+        }
+    });
+
+    btnLogout.addEventListener("click", async () => {
+        await signOut(auth);
+    });
+}
+
+// Registrar e login podem ser adicionados em modais ou páginas separadas
